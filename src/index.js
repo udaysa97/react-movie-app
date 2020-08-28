918c9d009e107c681453cb98cd0f369b97cbdeb3
udaysa97 <uday.sa97@gmail.com>
2020-08-29 00:40:01 +0530

    Added reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
+const store = createStore(movies);
+console.log('Before',store.getState());
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{
+    name:'superman'
+  }]
+});
+console.log('After',store.getState())
 ReactDOM.render(
     <App />,
   document.getElementById('root')
