17645c67693808a68db407281281713623b2791b
udaysa97 <uday.sa97@gmail.com>
2020-08-29 00:54:29 +0530

    pre store subscribe changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import {data}  from '../data';
 import Navbar from './Navbar'
 import MovieCard from './MovieCard'
+import {data} from '../data';
 
-function App() {
-  return (
-    <div className="App">
-     <Navbar />
-     <div className = "main">
-       <div className="tabs">
-         <div className = "tab">
-           Movie
+class App extends React.Component {
+  componentDidMount(){
+    this.props.store.dispatch({
+      type:'ADD_MOVIES',
+      movies: data
+    });
+    console.log('STATE',this.props.store.getState());
+  }
+  render() {
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">
+              Movie
          </div>
-         <div className = "tab">
-           Favourite
+            <div className="tab">
+              Favourite
          </div>
-       </div>
-       <div className = "list">
-         {data.map((movie,index) =>(
-           <MovieCard movie = {movie} key = {`movie-${index}`}/>
-         ))}
-       </div>
-     </div>
-    </div>
-  );
+          </div>
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movie-${index}`} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import App from './components/App';
 import movies from './reducers';
 
 const store = createStore(movies);
-console.log('Before',store.getState());
-store.dispatch({
-  type:'ADD_MOVIES',
-  movies:[{
-    name:'superman'
-  }]
-});
-console.log('After',store.getState())
+// console.log('Before',store.getState());
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{
+//     name:'superman'
+//   }]
+// });
+// console.log('After',store.getState())
 ReactDOM.render(
-    <App />,
+    <App store = {store}/>,
   document.getElementById('root')
 );
